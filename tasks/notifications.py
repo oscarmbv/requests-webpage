563a55417d6733df4f0c50cc11eea31cdc4096fb
Oscar Barrios <oscarmbv@gmail.com>
2025-06-29 10:50:06 -0400

    Fixed Slack notification issue

--- tasks/notifications.py
+++ tasks/notifications.py
@@ -370,9 +370,9 @@ def notify_new_request_created(request_pk, http_request_host=None, http_request_
                 scheduled_date_str = format_datetime_to_str(request_obj.scheduled_date, request_obj.requested_by)
                 additional_details_telegram.append(f"Scheduled for: {escape_markdown_v2(scheduled_date_str)}")
 
-            if request_obj.scheduled_date:
-                scheduled_date_str = format_datetime_to_str(request_obj.scheduled_date, request_obj.requested_by)
-                additional_details_telegram.append(f"Scheduled for: {escape_markdown_v2(scheduled_date_str)}")
+            if request_obj.status == UserRecordsRequest.status.PENDING_APPROVAL:
+                approval_msg = "The request needs approval from leadership before operate."
+                additional_details_telegram.append(f"Status: {escape_markdown_v2(approval_msg)}")
 
             type_display_escaped = escape_markdown_v2(type_display)
             req_code_escaped = escape_markdown_v2(request_obj.unique_code)
